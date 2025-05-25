ac84f5a2c0459d445a367e25adf49d410713b023
Ha Bach <muninnhugin@gmail.com>
2025-05-25 12:29:29 -0700

    delete unused import

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,5 +1,5 @@
 'use client'
-import React, {FunctionComponent, useEffect} from "react";
+import React, {useEffect} from "react";
 import Link from "next/link";
 import { usePathname } from 'next/navigation';
 
