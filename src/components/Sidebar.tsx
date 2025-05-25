6b95a07f0d074546e76da0b0f8732b94ca5fe3e8
Ha Bach <muninnhugin@gmail.com>
2025-05-24 20:02:46 -0700

    add side bar template

---
+++ src/components/Sidebar.tsx
@@ -0,0 +1,34 @@
+import { IoLogoGithub, IoLogoLinkedin } from "react-icons/io";
+import Image from "next/image";
+
+
+const Sidebar = () => {
+    return (
+        <div>
+            {/*TODO: find a funny picture to put here*/}
+            <Image
+                src="/images/avatar.png"
+                alt="My Profile"
+                width={128}
+                height={128}
+                className="mx-auto border rounded-full"
+            />
+            <h3 className="my-4 text-3xl font-bold">
+                Ha Bach</h3>
+            <p className="px-2 py-1 my-3 ">Software Developer</p>
+
+            {/*  social media icons  */}
+            <div className="flex justify-evenly my-5 items-center md:w-full">
+                {/*TODO: change GitHub pfp*/}
+                <a href="https://github.com/ha-bach">
+                    <IoLogoGithub className="w-8 h-8 cursor-pointer"/>
+                </a>
+                <a href="https://www.linkedin.com/in/ha-bach/">
+                    <IoLogoLinkedin className="w-8 h-8 cursor-pointer"/>
+                </a>
+            </div>
+        </div>
+    )
+}
+
+export default Sidebar;
